acc24e6678f4a6e269d759eab769e28080d0bc1a
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-09-25 12:27:16 +1000

    Specify provider version constraints in root module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = ">= 1.2.8"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+
     pass = {
       source  = "nicholas.cloud/nchlswhttkr/pass"
       version = "~> 0.1"
